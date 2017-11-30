bc520ec334a786758191840e38e56dff173bd600
JuCode <exme@exme.net>
2017-11-30 16:48:01 +0100

    - Fixed: Problem with IfChecked on the start.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,20 +17,165 @@ using System.Windows.Shapes;
 
 namespace PasswordGenerator
 {
-    /// <summary>
-    /// Interaktionslogik für MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            tb_Output_Characters.Text = tb_Output_Characters.Text + Options.smallLetter + Options.bigLetters + Options.numbers;
         }
 
         private void Generate_Click(object sender, RoutedEventArgs e)
         {
+            // Clean previous generated passwords from the memory
+            GC.Collect();
+
+            // The Lenght of the Generated Password
+            if ((cb_Password_Length.IsChecked is true) || (cb_Password_Length.IsChecked is false))
+            {
+                var check = int.TryParse(tb_Password_Length.Text, out var outputLenght);
+                if (check)
+                {
+                    // Set Value for the Lenght of the Generated Password
+                    Options.Laenge = outputLenght;
+                    // Final Generated Password
+                    tb_Generated_Password.Text = Options.CharGenerator(tb_Output_Characters.Text);
+                }
+                else
+                    MessageBox.Show("Value should be a Number.");
+            }
+
+            //Save To File CheckBox definition
+            if (cb_Save_To_File.IsChecked is true)
+            {
+                if (cb_Email.IsChecked is true & cb_Username.IsChecked is true)
+                    Options.AppendToAFile(Options.Pfad, tb_Generated_Password.Text + " " + tb_Username.Text + " " + tb_Email.Text);
+                else if (cb_Username.IsChecked is true)
+                    Options.AppendToAFile(Options.Pfad, tb_Generated_Password.Text + " " + tb_Username.Text + " " + null);
+                else if (cb_Email.IsChecked is true)
+                    Options.AppendToAFile(Options.Pfad, tb_Generated_Password.Text + " " + null + " " + tb_Email.Text);
+                else
+                    Options.AppendToAFile(Options.Pfad, tb_Generated_Password.Text + " " + null + " " + null);
+            }
+        }
+
+        private void cb_Save_To_File_Checked(object sender, RoutedEventArgs e)
+        {
+            if (cb_Save_To_File.IsChecked is true)
+            {
+                cb_Username.IsEnabled = true;
+                cb_Email.IsEnabled = true;
+            }
+            else
+            {
+                cb_Username.IsEnabled = false;
+                cb_Email.IsEnabled = false;
+            }
+        }
+
+        private void cb_Email_Checked(object sender, RoutedEventArgs e)
+        {
+            if (cb_Email.IsChecked is true)
+                tb_Email.IsEnabled = true;
+            else
+                tb_Email.IsEnabled = false;
+        }
+
+        private void cb_Username_Checked(object sender, RoutedEventArgs e)
+        {
+            if (cb_Username.IsChecked is true)
+                tb_Username.IsEnabled = true;
+            else
+                tb_Username.IsEnabled = false;
+        }
+
+        private void tb_Username_TextChanged(object sender, TextChangedEventArgs e)
+        {
 
         }
 
+        private void tb_Email_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Password_View_Click(object sender, RoutedEventArgs e)
+        {
+            // var view = new PasswordViewer();
+            if (File.Exists(Options.Pfad))
+            {
+                // view.toolStripStatusLabel1.Text = Options.Pfad.ToString();
+                // view.Show();
+            }
+            else MessageBox.Show("You need to check 'Save to a File' box\nand generate some passwords before\nyou can view anything.");
+
+        }
+
+        private void cb_Password_Length_Checked(object sender, RoutedEventArgs e)
+        {
+            if (cb_Password_Length.IsChecked is true)
+                tb_Password_Length.IsEnabled = true;
+            else
+                tb_Password_Length.IsEnabled = false;
+        }
+
+        private void tb_Password_Length_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void tb_Generated_Password_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void cb_Small_Letters_Checked(object sender, RoutedEventArgs e)
+        {
+            if (cb_Small_Letters.IsLoaded is true)
+            {
+                if (cb_Small_Letters.IsChecked is true)
+                    tb_Output_Characters.Text = tb_Output_Characters.Text + Options.smallLetter;
+                else
+                    tb_Output_Characters.Text = tb_Output_Characters.Text.Replace(Options.smallLetter, "");
+            }
+        }
+
+        private void cb_Big_Letters_Checked(object sender, RoutedEventArgs e)
+        {
+            if (cb_Big_Letters.IsLoaded is true)
+            { 
+                if (cb_Big_Letters.IsChecked is true)
+                tb_Output_Characters.Text = tb_Output_Characters.Text + Options.bigLetters;
+            else
+                tb_Output_Characters.Text = tb_Output_Characters.Text.Replace(Options.bigLetters, "");
+            }
+        }
+
+        private void cb_Numbers_Checked(object sender, RoutedEventArgs e)
+        {
+            if (cb_Numbers.IsLoaded is true)
+            {
+                if (cb_Numbers.IsChecked is true)
+                    tb_Output_Characters.Text = tb_Output_Characters.Text + Options.numbers;
+                else
+                    tb_Output_Characters.Text = tb_Output_Characters.Text.Replace(Options.numbers, "");
+            }
+        }
+
+        private void cb_Symbols_Checked(object sender, RoutedEventArgs e)
+        {
+            if (cb_Symbols.IsLoaded is true)
+            {
+                if (cb_Symbols.IsChecked is true)
+                    tb_Output_Characters.Text = tb_Output_Characters.Text + Options.symbols;
+                else
+                    tb_Output_Characters.Text = tb_Output_Characters.Text.Replace(Options.symbols, "");
+            }
+        }
+
+        private void tb_Output_Characters_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
---
+++ Options.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO;
+
+namespace PasswordGenerator
+{
+    class Options
+    {
+        // Write to a file
+        public static string Pfad = Directory.GetCurrentDirectory() + "\\pass.txt";
+        public static List<string> PassList = new List<string>();
+
+        // The Lenght of the Generated Password set to Default
+        private static int _laenge = 12;
+        public static int Laenge
+        {
+            get => _laenge;
+            set
+            {
+                if (value <= 0)
+                    MessageBox.Show("Value should not be NULL.\nPrevious Value will be used.");
+                else
+                    _laenge = value;
+            }
+        }
+
+
+        // Counter
+        private static short counter = 0;
+        public static short Counter { get => counter; set => counter = value; }
+       
+
+
+        // Character Generator and Password output
+        // Default Characters have been taken from TextBox
+        public static string bigLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public static string smallLetter = "abcdefghijklmnopqrstuvwxyz";
+        public static string numbers = "0123456789";
+        public static string symbols = "()`~!@#$%^&*-+=|{}[]:;\"\'<>,.?/";
+
+        public static string CharGenerator(string Characters)
+        {
+            MainWindow form1 = new MainWindow();
+            // TextBox Text convert to String
+            //string chars = form1.tb_zeichen.Text;
+
+            char[] stringChars = new char[Laenge];
+            Random random = new Random();
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = Characters[random.Next(Characters.Length)];
+            }
+            string finalString = new String(stringChars);
+            return finalString;
+        }
+
+
+        // Method to append text to the existing file with existing text/lines
+        public static void AppendToAFile(string pfad, string output)
+        {
+            PassList.Add(output); // Just to result the use of the AppendLine and not AppendText
+            File.AppendAllLines(pfad, PassList); // Add Line to the Textfile
+            PassList.Remove(output);
+        }
+    }
+}
